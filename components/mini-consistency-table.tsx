4cb76b268b67290472773416553d3261d4608b9a
Murtaza Shiyaji <87291205+MShiyaji@users.noreply.github.com>
2025-05-21 14:57:12 -0700

    another UI change

--- components/mini-consistency-table.tsx
+++ components/mini-consistency-table.tsx
@@ -18,9 +18,10 @@ export function MiniConsistencyTable({ players, filterName }) {
         <table className="w-full text-xs">
           <thead className="text-gray-400">
             <tr>
-              <th className="w-6 text-left py-1">#</th>
-              <th className="text-left py-1">Player</th>
-              <th className="text-right py-1">%</th>
+              <th className="w-5 text-left py-1">#</th>
+              <th className="text-left py-1 w-[38%]">Player</th>
+              <th className="text-right py-1 w-[25%]">Events</th>
+              <th className="text-right py-1 w-[15%]">%</th>
             </tr>
           </thead>
           <tbody className="text-gray-300">
@@ -32,7 +33,10 @@ export function MiniConsistencyTable({ players, filterName }) {
               return (
                 <tr key={idx} className="border-t border-gray-800">
                   <td className="py-1">{idx + 1}</td>
-                  <td className="py-1 font-medium truncate" style={{maxWidth: "120px"}}>{player.name}</td>
+                  <td className="py-1 font-medium truncate" style={{maxWidth: "90px"}}>{player.name}</td>
+                  <td className="py-1 text-right">
+                    {player.events || player.numEvents || "N/A"}
+                  </td>
                   <td className="text-right py-1">{player.consistency}</td>
                 </tr>
               );
